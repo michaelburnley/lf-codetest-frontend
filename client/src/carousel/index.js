c22f1c14f3cbc7d23e66cd6f2b45b7c71c553fa7
Michael Burnley <michael@cybercowgames.com>
2018-10-03 15:38:55 -0700

    ejected react project and added carousel base

---
+++ client/src/carousel/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Carousel extends Component {
+  constructor() {
+    super();
+    this.state = {
+      slide: 1
+    };
+  }
+
+  render() {
+    return(
+      <div>
+      </div>
+    );
+  }
+
+}
+
+export default Carousel;
