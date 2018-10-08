56fbdcc926fb3707046890f5ddbfda5166fad445
Michael Burnley <michael@cybercowgames.com>
2018-10-08 15:46:24 -0700

    finishing touches on functionality

--- client/src/carousel/index.js
+++ client/src/carousel/index.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 
-const Slide = ({ image, title, subtitle, pauseSlides }) => {  
+const Slide = ({ data, pauseSlides, translate }) => {  
+  const styles = { 
+    transform: `translateX(-${translate}%`,
+  };
   return (
-    <div onMouseOver={pauseSlides} className="slide">
-      <div className="text-blocks">
-        <div className="title">{title}</div>
-        <div className="subtitle">{subtitle}</div>
-      </div>
-      <img src={image} alt={title}/>
+    <div onMouseOver={pauseSlides} onMouseLeave={pauseSlides} className="slide" style={styles}>
+      <TextBlocks data={data}/>
+      <img src={data.image} alt={data.title}/>
     </div>
   )
 }
+
+const TextBlocks = ({ data: { title, subtitle }}) => (
+    <div className="text-blocks">
+      <div className="title">{title}</div>
+      <div className="subtitle">{subtitle}</div>
+    </div>
+  )
 
 const SlideIndicator = ({ current_slide, timer, onClick }) => {
   return(
     <div id="indicator" onClick={onClick}>
       <div>Current Slide: {current_slide + 1}</div>
-      <div>Time to Change: {timer}</div>
+      <div>Time to Change: {timer / 1000}s</div>
     </div>
   )
 }
@@ -36,7 +43,9 @@ class Carousel extends Component {
     this.state = {
       slide: 0,
       interval: 5000,
+      timeLeft: 5000,
       paused: false,
+      translate: 0,
       images: [
         {
           image: "/slide1.jpg",
@@ -59,31 +68,71 @@ class Carousel extends Component {
     this.prevSlide = this.prevSlide.bind(this);
     this.pauseSlides = this.pauseSlides.bind(this);
     this.goToSlide = this.goToSlide.bind(this);
+    this.timer = this.timer.bind(this);
+    this.clearTimers = this.clearTimers.bind(this);
+    this.restartTimers = this.restartTimers.bind(this);
   }
 
   componentDidMount() {
-    setInterval(() => {
+    this.timer();
+  }
+
+  timer() {
+    this.sliderInterval = setInterval(() => {
       this.nextSlide();
     }, this.state.interval);
+    this.countdown = setInterval(() => {
+      this.setState({ timeLeft: this.state.timeLeft - 1000})
+    }, 1000);
+  }
+
+  clearTimers() {
+    clearInterval(this.sliderInterval);
+    clearInterval(this.countdown);
+    this.setState({ timeLeft: this.state.interval });
+  }
+
+  restartTimers() {
+    this.clearTimers();
+    this.timer();
   }
 
   pauseSlides() {
-    console.log("paused")
-    this.setState({ paused: true });
+    if(!this.state.paused) {
+      this.clearTimers();
+    } else {
+      this.timer();
+    }
+    this.setState({ paused: !this.state.paused });
   }
 
   nextSlide() {
-    let a = this.state.slide + 1;
-    a < this.state.images.length ? this.setState({ slide: a }) : this.setState({ slide: 0 });
+    this.restartTimers();
+    let state = {
+      slide: this.state.slide + 1,
+      translate: this.state.translate
+    };
+    state.slide < this.state.images.length ? state.translate += 100 : state.slide = state.translate = 0;
+    this.setState(state);
   }
 
   prevSlide() {
-    let a = this.state.slide - 1;
-    a < 0 ? this.setState({ slide: this.state.images.length - 1 }) : this.setState({ slide: a });
+    this.restartTimers();
+    let state = {
+      slide: this.state.slide - 1,
+      translate: this.state.translate
+    }
+    state.translate === 0 && state.slide < 0 ? state = { translate: 200, slide: this.state.images.length -1} : state.translate -= 100;
+    this.setState(state);
   }
 
   goToSlide(slideNumber) {
-    this.setState({ slide: slideNumber });
+    this.restartTimers();
+    let state = { 
+      slide: slideNumber,
+      translate: slideNumber * 100 
+    }
+    this.setState(state);
   }
 
   render() {
@@ -102,6 +151,7 @@ class Carousel extends Component {
             <SlideIndicator
               current_slide={this.state.slide} 
               key={i}
+              timer={this.state.timeLeft}
               onClick={e => this.goToSlide(i)} />
           ))
         }
@@ -111,10 +161,9 @@ class Carousel extends Component {
           {
             this.state.images.map((data, i) => (
               <Slide 
-                image={data['image']}
-                title={data['title']} 
-                subtitle={data['subtitle']} 
+                data={data}
                 pauseSlides={this.pauseSlides}
+                translate={this.state.translate}
                 key={i} />
             ))
           }
