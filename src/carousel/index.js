5144d519c2bfd235d24e4fbfec8a6ecf5400349a
Michael Burnley <michael@cybercowgames.com>
2018-10-08 23:02:50 -0700

    css cleanup

--- src/carousel/index.js
+++ src/carousel/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import './carousel.scss';
+
 
 // Stateless Components
 
